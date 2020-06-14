ae70c02689b82ada4de9d0bfb8d9e81ea38d54f8
Rafli Firdausy Irawan <rafly.firdausy@gmail.com>
2020-06-14 16:08:40 +0700

    DONE GANTI PASSWORD

--- pegawai/application/controllers/Ganti_password.php
+++ pegawai/application/controllers/Ganti_password.php
@@ -6,6 +6,7 @@ class Ganti_password extends Admin_Controller
     public function __construct()
     {
         parent::__construct();
+        $this->load->model("Admin_model", "admin");
     }
 
     public function index()
@@ -17,36 +18,35 @@ class Ganti_password extends Admin_Controller
     {
         $password_sekarang      = $this->input->post('password_sekarang');
         $password_baru          = $this->input->post('password_baru');
-        $konfirmasi_password    = $this->input->post('konfirmasi_password');
-        $id_user                = $this->global_data['user_data']->id_user;
+        $konfirmasi_password    = $this->input->post('konfirmasi_password');        
 
-        $cekCredential  = $this->m_data->getWhere("id_user", $id_user);
-        $cekCredential  = $this->m_data->getWhere("password_user", md5($password_sekarang));
-        $cekCredential  = $this->m_data->getData("user")->row();
+        $cekCredential  = $this->admin->where([
+            "id_admin"          => $this->session->userdata("login_pegawai")->id_admin,
+            "password_admin"    => md5($password_sekarang)
+        ])->get();        
 
         $sukses = FALSE;
 
         if ($cekCredential) {
-            if($password_baru == $konfirmasi_password){
-                $update = $this->m_data->update(
-                    "user",
-                    ["password_user" => md5($password_baru)],
-                    ["id_user" => $id_user]
+            if ($password_baru == $konfirmasi_password) {
+                $update = $this->admin->update(
+                    ["password_admin" => md5($password_baru)],
+                    $this->session->userdata("login_pegawai")->id_admin
                 );
-                if($update > 0){
+                if ($update) {
                     $sukses = TRUE;
-                    $this->session->set_flashdata("sukses", "Berhasil mengubah password pegawai pada database. Silahkan Masuk dengan password yang baru");
+                    $this->session->set_flashdata("sukses", "Berhasil mengubah password pada database. Silahkan Masuk dengan password yang baru");
                 } else {
-                    $this->session->set_flashdata("gagal", "Gagal mengubah password pegawai pada database");
+                    $this->session->set_flashdata("gagal", "Gagal mengubah password pada database");
                 }
             } else {
                 $this->session->set_flashdata("gagal", "Konfirmasi password salah");
-            }        
+            }
         } else {
             $this->session->set_flashdata("gagal", "Password lama yang kamu masukan salah");
         }
 
-        if($sukses){
+        if ($sukses) {
             $this->session->unset_userdata('login_pegawai');
             redirect(base_url("auth/login"));
         } else {
